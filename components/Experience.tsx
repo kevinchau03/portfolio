64f681fc7dca783d69c7ac6f919b4827bb516b8c
Kevin Chau <kchau57@uwo.ca>
2024-07-28 20:18:39 -0400

    Experience section improvements

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,42 +1,96 @@
-'use client'
+'use client';
 import React from 'react';
 import { motion } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 
+const experience = [
+  {
+    title: 'Software Engineering Intern @ HitMeUp.ai',
+    date: 'May 2024 - PRESENT',
+    description: (
+      <ul className="list-disc pl-5">
+        <li>Developed a web application using Django</li>
+        <li>Implemented a chat widget feature using JavaScript that can be embedded on different websites</li>
+        <li>Designed and developed the settings page dealing with front end and backend capabilities</li>
+      </ul>
+    ),
+  },
+  {
+    title: 'Senior Director of Communications @ Western AI',
+    date: '2022 - 2024',
+    description: (
+      <ul className="list-disc pl-5">
+        <li>Developed a web application for external and internal members to connect</li>
+        <li>Implemented a chat feature using Firebase Cloud Messaging</li>
+        <li>Designed and developed a RESTful API using Node.js and Express.js</li>
+      </ul>
+    ),
+  },
+  {
+    title: 'Python Tutor @ ScratchCoding4Kids',
+    date: '2022',
+    description: (
+      <ul className="list-disc pl-5">
+        <li>Developed a mobile application using React Native and Firebase</li>
+        <li>Implemented a chat feature using Firebase Cloud Messaging</li>
+        <li>Designed and developed a RESTful API using Node.js and Express.js</li>
+      </ul>
+    ),
+  },
+];
+
+const skills = [
+  { skill: 'Python' },
+  { skill: 'JavaScript' },
+  { skill: 'Django' },
+  { skill: 'React' },
+  { skill: 'Next.js' },
+  { skill: 'PostgreSQL' },
+];
+
+const variants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: { opacity: 1, x: 0 },
+};
+
 export function Experience() {
   return (
-    <div id="experience" className="flex flex-col h-screen justify-center w-full">
-    <div className="flex items-center justify-center mb-4">
-      <h1 className="text-5xl font-bold flex-shrink-0">
-        <span className="text-sky-800">01. </span>
-        My Experience
-      </h1>
-      <span className="flex-1 border-b border-black ml-4"></span>
-    </div>
-      <div id="job-1" className="flex rounded-lg cursor-pointer hover:bg-slate-100 hover:border-gray-100 hover:shadow-2xl p-2">
-        <h3 className="text-slate-400">2024 - PRESENT</h3>
-        <div className="flex flex-col">
-          <h1 className="font-bold text-lg">Software Engineer Intern • HitMeUp.ai</h1>
-          <p className="text-slate-400">Currently working as a Software Engineering Intern at HitMeUp.ai. I work on the backend, specifically on the API and database. I also work on the frontend, specifically on the dashboard and user interface.</p>
-          <div className="flex justify-between">
-              <div className="bg-sky-400 font-bold text-sm rounded-lg border p-1">JavaScript</div>
-            <div className="bg-sky-400 font-bold text-sm rounded-lg border p-1">Python</div>
-            <div className="bg-sky-400 font-bold text-sm rounded-lg border p-1">PostgreSQL</div>
-            <div className="bg-sky-400 font-bold text-sm rounded-lg border p-1">JavaScript</div>
-          </div>
+    <div id="experience" className="flex flex-col lg:flex-row items-center justify-center h-screen px-4">
+      <div className="w-full lg:w-1/2 lg:px-4">
+        <h1 className="font-bold text-2xl mb-6 text-center lg:text-left">01. Experience</h1>
+        <div>
+          {experience.map((item, index) => {
+            const { ref, inView } = useInView({
+              triggerOnce: false,
+              threshold: 0.5,
+            });
+
+            return (
+              <motion.div
+                key={`experience-${index}`}
+                ref={ref}
+                initial="hidden"
+                animate={inView ? 'visible' : 'hidden'}
+                variants={variants}
+                transition={{ duration: 1, ease: 'easeOut' }}
+                className="mb-10"
+              >
+                <h2 className="text-xl text-black">{item.title}</h2>
+                <span className="w-fit rounded-full py-1 text-sm italic text-neutral-800">{item.date}</span>
+                <div className="mt-2 text-sm text-black">{item.description}</div>
+              </motion.div>
+            );
+          })}
         </div>
       </div>
-      <div id="job-2" className="flex rounded-lg cursor-pointer hover:bg-slate-100 hover:border-gray-100 hover:shadow-2xl p-2">
-        <h3 className="text-slate-400">2022 - 2024</h3>
-        <div className="flex flex-col">
-          <h1 className="font-bold text-lg">Director of Communications • Western AI</h1>
-          <p className="text-slate-400">Currently working as a Software Engineering Intern at HitMeUp.ai. I work on the backend, specifically on the API and database. I also work on the frontend, specifically on the dashboard and user interface.</p>
-          <div className="flex justify-between">
-              <div className="bg-sky-400 font-bold text-sm rounded-lg border p-1">JavaScript</div>
-            <div className="bg-sky-400 font-bold text-sm rounded-lg border p-1">Python</div>
-            <div className="bg-sky-400 font-bold text-sm rounded-lg border p-1">PostgreSQL</div>
-            <div className="bg-sky-400 font-bold text-sm rounded-lg border p-1">Figma</div>
-          </div>
+      <div className="w-full lg:w-1/2 lg:px-4 mt-10 lg:mt-0">
+        <h1 className="font-bold text-2xl mb-6 text-center lg:text-left">Skills</h1>
+        <div className="grid grid-cols-2 gap-4 items-center justify-center">
+          {skills.map((item, index) => (
+            <div key={`skill-${index}`} className="border-2 border-black rounded-lg p-4 font-bold text-center hover:text-sky-700">
+              <h2 className="text-xl text-black">{item.skill}</h2>
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const Nav = () => {
   return (
-    <div className="fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 border-2 bg-white border-black rounded-full px-8 py-3">
+    <div className="fixed top-3 inset-x-0 max-w-2xl mx-auto z-50 border-2 bg-white border-black rounded-full px-8 py-3">
       <div className="flex justify-between gap-6">
         <Link href="#hero" className="hover:text-gray-300 font-bold text-xl">
           Kevin Chau
