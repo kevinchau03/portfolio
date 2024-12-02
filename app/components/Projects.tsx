af02512630847c9c3699e1a12fb1bcc15620dc45
Kevin Chau <kchau57@uwo.ca>
2024-12-02 16:21:18 -0500

    logo issue

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -41,7 +41,9 @@ export function Projects() {
                           transition duration-300 ease-in-out">
             <div className="flex items-center mb-3">
               <div className="w-12 h-12 mr-3 inline-flex items-center justify-center rounded-full bg-blue-600 text-white flex-shrink-0">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-newspaper"><path d="M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-2 2Zm0 0a2 2 0 0 1-2-2v-9c0-1.1.9-2 2-2h2"/><path d="M18 14h-8"/><path d="M15 18h-5"/><path d="M10 6h8v4h-8V6Z"/></svg>              
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-newspaper"><path d="M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-2 2Zm0 0a2 2 0 0 1-2-2v-9c0-1.1.9-2 2-2h2" />
+                  <path d="M18 14h-8" /><path d="M15 18h-5" /><path d="M10 6h8v4h-8V6Z" />
+                </svg>
               </div>
               <h2 className="text-lg font-medium">Uplifted Echoes</h2>
             </div>
