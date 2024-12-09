9928f2579c2ee0445c7a795808faede1728ce54c
Kevin Chau <kchau57@uwo.ca>
2024-12-09 16:54:51 -0500

    mobile optimization for about me page

--- app/passion/page.tsx
+++ app/passion/page.tsx
@@ -3,19 +3,22 @@ import Image from 'next/image';
 
 export default function Page() {
     return (
-        <main className="flex flex-col justify-center gap-4 mt-24">
+        <main className="flex flex-col justify-center gap-4 mt-20">
             <div className="text-left mb-4">
-                <h1 className="text-3xl font-bold mb-4">Hey there!</h1>
-                <p className="mb-4">
+                <div className="flex items-center">
+                    <h1 className="text-3xl font-bold mb-4">Hey there!</h1>
+                    <Image src="/IMG_4551.png" alt="me" width={100} height={100} className="rounded-full" />
+                </div>
+                <p className="mb-4 text-sm md:text-base">
                     My name is Kevin Chau, an aspiring Software Engineer.
                     My passion for coding started when I discovered a beautiful site called "YouTube to MP3"—a godsend for my younger self.
                 </p>
-                <p className="mb-4">
+                <p className="mb-4 text-sm md:text-base">
                     Fast forward to high school: I took my first computer science course in grade 12 and was immediately hooked.
                     Here I am now, a fourth-year computer science student at Western University. I'm most passionate about full-stack development,
                     particularly building web and mobile apps from the ground up.
                 </p>
-                <div className="flex mb-4 bg-card rounded-xl p-4">
+                <div className="flex mb-4 text-sm md:text-base bg-card rounded-xl p-4">
                     <div className="bg-accent w-[5px] rounded-full"></div>
                     <div className="ml-4">
                         <p className="font-semibold mb-2">My current tech stack consists of:</p>
@@ -27,12 +30,12 @@ export default function Page() {
                         </ul>
                     </div>
                 </div>
-                <p className="mb-4">
+                <p className="mb-4 text-sm md:text-base">
                     Away from my laptop, I enjoy reading comic books, playing basketball, and self-improvement.
-                    I like to live by a japanese word called "Kaizen,". By abiding by this rule, I am always looking for 
+                    I like to live by a japanese word called "Kaizen,". By abiding by this rule, I am always looking for
                     ways to improve myself and my work.
                 </p>
-                <div className="flex mb-4 bg-card rounded-xl p-4">
+                <div className="flex mb-4 text-sm md:text-base bg-card rounded-xl p-4">
                     <div className="bg-accent w-[5px] rounded-full"></div>
                     <div className="ml-4">
                         <p className="font-bold text-lg">Kaizen</p>
@@ -43,16 +46,16 @@ export default function Page() {
                         </p>
                     </div>
                 </div>
-                <p className="mb-4">
+                <p className="mb-4 text-sm md:text-base">
                     My biggest motivators are my friends and family. They are very important pieces in my life and have shaped me into who I am today.
                     Here are some photos as my appreciation. (Psst. Don't let them know how cheesy I am!)
                 </p>
-                <div className="grid gap-4 lg:grid-cols-3 mb-4 bg-card p-4 rounded-xl">
-                    <Image src="/lantern.webp" alt="lantern" width={375} height={375} />
-                    <Image src="/beach.webp" alt="beach" width={375} height={375} />
-                    <Image src="/family.webp" alt="family" width={375} height={375} />
+                <div className="grid gap-4 items-center justify-center lg:grid-cols-3 mb-4 text-sm md:text-base bg-card p-4 rounded-xl">
+                    <Image src="/lantern.webp" alt="lantern" width={375} height={375} className="rounded-xl" />
+                    <Image src="/beach.webp" alt="beach" width={375} height={375} className="rounded-xl" />
+                    <Image src="/family.webp" alt="family" width={375} height={375} className="rounded-xl" />
                 </div>
-                <p className="mb-4">
+                <p className="mb-4 text-sm md:text-base">
                     If you made it this far in the website, I just to say thank you for getting this far. I hope you got to learn a little bit
                     more about me. Feel free to shoot me a message and connect with me for a quick chat.
                 </p>
