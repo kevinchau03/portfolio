7d9e0c900c071eabcfbd7245e441ba17a56fc596
Kevin Chau <kchau57@uwo.ca>
2025-08-14 18:33:19 -0400

    feat: new project showcase

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -57,6 +57,30 @@ export function Projects() {
         animate={isVisible ? "visible" : "hidden"}
         ref={ref}
       >
+        <motion.div variants={projectVariants} className="max-w-md">
+          <div className="flex rounded-lg h-full p-8 flex-col border border-black dark:bg-card
+                          transform hover:-translate-y-2 hover:shadow-[8px_8px_0px_0px_rgba(0,0,0,0.8)] dark:hover:dark:shadow-white/20
+                          transition duration-300 ease-in-out">
+            <div className="flex items-center mb-3">
+              <div className="w-12 h-12 mr-3 inline-flex items-center justify-center rounded-full bg-primary text-white flex-shrink-0">
+                <Image src="/Eleet.webp" alt="Eleet" width={100} height={100} className="rounded-full" />
+              </div>
+              <h2 className="text-lg font-bold">Eleet Discord Bot</h2>
+            </div>
+            <div className="flex flex-col justify-between flex-grow">
+              <p className="leading-relaxed text-muted">
+                A discord bot that helps discord users grind leetcode!
+              </p>
+              <a href="https://github.com/kevinchau03/leetcode-bot" target="_blank" className="mt-3 text-muted hover:text-primary inline-flex items-center">
+                Learn More
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                  strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                  <path d="M5 12h14M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </motion.div>
         {/* Project 1 */}
         <motion.div variants={projectVariants} className="max-w-md">
           <div className="flex rounded-lg h-full p-8 flex-col border border-black dark:bg-card
@@ -101,36 +125,6 @@ export function Projects() {
                 A real-time news website that provides the latest news from around the world.
               </p>
               <a href="https://news-app-seven-rose.vercel.app/" target="_blank" className="mt-3 text-muted hover:text-primary inline-flex items-center">
-                Learn More
-                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
-                  strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-                  <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
-            </div>
-          </div>
-        </motion.div>
-        {/* Repeat for Project 3 */}
-        <motion.div variants={projectVariants} className="max-w-md">
-          <div className="flex rounded-lg h-full p-8 flex-col border border-black dark:bg-card
-                          transform hover:-translate-y-2 hover:shadow-[8px_8px_0px_0px_rgba(0,0,0,0.8)] dark:hover:dark:shadow-white/20
-                          transition duration-300 ease-in-out">
-            <div className="flex items-center mb-3">
-              <div className="w-12 h-12 mr-3 inline-flex items-center justify-center rounded-full bg-primary text-white flex-shrink-0">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-clapperboard">
-                  <path d="M20.2 6 3 11l-.9-2.4c-.3-1.1.3-2.2 1.3-2.5l13.5-4c1.1-.3 2.2.3 2.5 1.3Z" />
-                  <path d="m6.2 5.3 3.1 3.9" />
-                  <path d="m12.4 3.4 3.1 4" />
-                  <path d="M3 11h18v8a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2Z" />
-                </svg>
-              </div>
-              <h2 className="text-lg font-bold">Anime Buddy</h2>
-            </div>
-            <div className="flex flex-col justify-between flex-grow">
-              <p className="leading-relaxed text-muted">
-                A chrome extension that lets users keep track of their favourite shows and movies.
-              </p>
-              <a href="https://github.com/kevinchau03/AnimeBuddy" target="_blank" className="mt-3 text-muted hover:text-primary inline-flex items-center">
                 Learn More
                 <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
                   strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
